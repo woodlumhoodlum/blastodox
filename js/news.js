2f787f2e3f0041f281e8ae6172dd7e545d7b06dc
Jonathan Davis <jonathankeithdavis@yahoo.com>
2015-07-12 21:02:12 -1000

    fixins

--- js/news.js
+++ js/news.js
@@ -1,35 +1,105 @@
-$(document).ready(function(){
-	var template = $("#politicsTemplate").html();
-    var renderer = Handlerbars.compile(template);
-    var result = renderer({ "newsItem" : [{
-     "color" : "white",
-     "description" : "This is an awesome news article",
-     "date" : "4/4/2014",
-     "link" : "www.google.com"
-     }]
-    });
-    
-    var isRed = function() {
-	  return this.get('color') === 'red';
-	}.property('color');
-  var isWhite = function() {
-	  return this.get('color') === 'white';
-	}.property('color');
-  var isBlue = function() {
-	  return this.get('color') === 'blue';
-	}.property('color');
+$(document).ready(function() {
+	//register helpers
+	Handlebars.registerHelper("isRed", function(newsItem){
+		if(newsData.color==='red'){
+			return true;
+		} else {
+			return false;
+		}
+		
+	});
+	Handlebars.registerHelper("isWhite", function(newsItem){
+		if(newsData.color==='white'){
+			return true;
+		} else {
+			return false;
+		}
+		
+	});
+	Handlebars.registerHelper("isBlue", function(newsItem){
+		if(newsData.color==='blue'){
+			return true;
+		} else {
+			return false;
+		}
+		
+	});
 
-    
-    
- 
+	var template = $("#politicsTemplateRed").html();
+	var renderer = Handlebars.compile(template);
+	var result = renderer({
+		"newsItem" : [{
+			"color" : "red",
+			"description" : "This is an awesome news article",
+			"date" : "4/4/2014",
+			"link" : "www.google.com"
+		}, {
+			"color" : "white",
+			"description" : "This is an awesome news article",
+			"date" : "4/4/2014",
+			"link" : "www.google.com"
+		}, {
+			"color" : "blue",
+			"description" : "This is an awesome news article",
+			"date" : "4/4/2014",
+			"link" : "www.google.com"
+		}]
+	});
 
+	$("#containerRed").html(result);
 
-     $(".politics-toggle").click(function(){
-     	if($(".politics").is(":visible")){
-         $(".politics").hide();
-     	} else {
-     		$(".politics").show();
-     	}
-     });
+	var template = $("#politicsTemplateWhite").html();
+	var renderer = Handlebars.compile(template);
+	var result = renderer({
+		"newsItem" : [{
+			"color" : "red",
+			"description" : "This is an awesome news article",
+			"date" : "4/4/2014",
+			"link" : "www.google.com"
+		}, {
+			"color" : "white",
+			"description" : "This is an awesome news article",
+			"date" : "4/4/2014",
+			"link" : "www.google.com"
+		}, {
+			"color" : "blue",
+			"description" : "This is an awesome news article",
+			"date" : "4/4/2014",
+			"link" : "www.google.com"
+		}]
+	});
 
- }); 
+	$("#containerWhite").html(result);
+
+	var template = $("#politicsTemplateBlue").html();
+	var renderer = Handlebars.compile(template);
+	var result = renderer({
+		"newsItem" : [{
+			"color" : "red",
+			"description" : "This is an awesome news article",
+			"date" : "4/4/2014",
+			"link" : "www.google.com"
+		}, {
+			"color" : "white",
+			"description" : "This is an awesome news article",
+			"date" : "4/4/2014",
+			"link" : "www.google.com"
+		}, {
+			"color" : "blue",
+			"description" : "This is an awesome news article",
+			"date" : "4/4/2014",
+			"link" : "www.google.com"
+		}]
+	});
+
+	$("#containerBlue").html(result);
+	$(".politics-toggle").click(function() {
+		if ($(".politics").is(":visible")) {
+			$(".politics").hide();
+		} else {
+			$(".politics").show();
+		}
+	});
+
+});
+
